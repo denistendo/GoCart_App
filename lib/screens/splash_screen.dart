601897b7d9fee499b6bb76a61bae5cb6376f7923
denistendo <ssebyaladenis18@gmail.com>
2025-06-11 09:56:56 +0300

    created the splash screen code using a stateful widget

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color:Colors.white,
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(
+              Icons.shopping_cart,
+              size:100,
+              color: Colors.blue,
+            ),
+            const SizedBox(height:20),
+            const Text(
+              'GoCart',
+              style: TextStyle(fontSize:30, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height:10),
+            const Text('Your one-stop shopping aplatform'),
+            
+          ]
+        )
+      ),
+    );
+  }
+}
