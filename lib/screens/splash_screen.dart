4b9b18e2deb9b7c498469aa120a94a6c38eca187
denistendo <ssebyaladenis18@gmail.com>
2025-06-11 10:48:06 +0300

    Included Sign Up button and Sign In text button

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -12,26 +12,47 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color:Colors.white,
+        color: Colors.white,
         alignment: Alignment.center,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Icon(
               Icons.shopping_cart,
-              size:100,
+              size: 100,
               color: Colors.blue,
             ),
-            const SizedBox(height:20),
+            const SizedBox(height: 20),
             const Text(
               'GoCart',
-              style: TextStyle(fontSize:30, fontWeight: FontWeight.bold),
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
             ),
-            const SizedBox(height:10),
-            const Text('Your one-stop shopping aplatform'),
-            
-          ]
-        )
+            const SizedBox(height: 10),
+            const Text('Your one-stop shopping platform'),
+            const SizedBox(height: 50),
+
+            ElevatedButton(
+              onPressed: () {
+                // Later: Navigate to Sign Up Screen
+              },
+              child: const Text('Sign Up'),
+            ),
+            const SizedBox(height: 20),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text("Already have an account?"),
+                TextButton(
+                  onPressed: () {
+                    // Later: Navigate to Sign In Screen
+                  },
+                  child: const Text("Sign In"),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
